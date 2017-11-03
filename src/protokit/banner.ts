a526d25ac3d92d506ff0e1e730cc6c8eae6a382f
tav <tav@espians.com>
2017-11-03 23:09:35 +0000

    protokit/banner: add a figlet-generated ascii logo.

---
+++ src/protokit/banner.ts
@@ -0,0 +1,14 @@
+// Public Domain (-) 2017-present The Component Kit Authors.
+// See the Component Kit UNLICENSE file for details.
+
+/* tslint:disable:no-trailing-whitespace */
+
+export const Logo = `
+                     _        _    _ _   
+     _ __  _ __ ___ | |_ ___ | | _(_) |_ 
+    | '_ \\| '__/ _ \\| __/ _ \\| |/ / | __|
+    | |_) | | | (_) | || (_) |   <| | |_ 
+    | .__/|_|  \\___/ \\__\\___/|_|\\_\\_|\\__|
+    |_|                                  
+
+`
