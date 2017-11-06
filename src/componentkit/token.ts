791d4b2348275c049c67e942ed414f27ca53fcfa
tav <tav@espians.com>
2017-11-06 16:41:06 +0000

    componentkit/token: stub package for token types.

---
+++ src/componentkit/token.ts
@@ -0,0 +1,15 @@
+// Public Domain (-) 2017-present The Component Kit Authors.
+// See the Component Kit UNLICENSE file for details.
+
+//! Package token defines the various token types.
+
+enum token {
+	HTML_TAG,
+	HTML_TAG_CLOSE_LEFT_DELIMITER,
+	HTML_TAG_CLOSE_RIGHT_DELIMITER,
+	HTML_TAG_OPEN_LEFT_DELIMITER,
+	HTML_TAG_OPEN_RIGHT_DELIMITER,
+	TEXT,
+}
+
+export default token
