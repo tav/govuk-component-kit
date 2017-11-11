9eac3ee32d8e486c89adc083537b930c5b4721f1
tav <tav@espians.com>
2017-11-11 15:44:49 +0000

    io: specify the base context interface.

---
+++ src/io.ts
@@ -0,0 +1,7 @@
+// Public Domain (-) 2017-present The Component Kit Authors.
+// See the Component Kit UNLICENSE file for details.
+
+// `Context` represents an I/O deadline.
+export interface Context {
+	deadline: number
+}
