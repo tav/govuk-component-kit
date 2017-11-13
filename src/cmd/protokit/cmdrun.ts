8a83922fc57adeedb66a78080f4720a31a96b091
tav <tav@espians.com>
2017-11-13 18:05:59 +0000

    cmd/protokit: implement a root renderer

--- src/cmd/protokit/cmdrun.ts
+++ src/cmd/protokit/cmdrun.ts
@@ -31,28 +31,38 @@ function createRouter(root: string) {
 	const mgr = new component.Manager(root)
 	return (ctx: web.Context) => {
 		const args = ctx.url.args
+		if (!args[0]) {
+			return serveRoot(ctx, mgr)
+		}
 		if (args[0] === 'static') {
 			return serveStatic(ctx, mgr, args.slice(1))
 		}
-		let page
+		let pageRenderer
 		try {
-			page = mgr.getPage(args)
+			pageRenderer = mgr.getPageRenderer(args)
 		} catch (err) {
 			log.error(err)
 			return 500
 		}
-		if (!page) {
+		if (!pageRenderer) {
 			return 404
 		}
 		let resp
 		try {
-			resp = page.render(ctx, mgr)
+			resp = pageRenderer(ctx, mgr)
 		} catch (err) {
 			log.error(err)
 			return 500
 		}
 		return resp
 	}
+}
+
+function getDate(timestamp: number) {
+	return new Date(timestamp)
+		.toISOString()
+		.slice(0, 10)
+		.replace(/-/g, '/')
 }
 
 function getDefaultPort() {
@@ -76,6 +86,78 @@ function getProtokitRoot() {
 	}
 }
 
+function serveRoot(ctx: web.Context, mgr: component.Manager) {
+	const cfgPath = path.join(mgr.root, 'site.js')
+	const cfgFile = fs.readFileSync(cfgPath, {encoding: 'utf8'})
+	const module = {exports: {Org: '', Title: ''}}
+	new Function('module', 'exports', cfgFile)(module, module.exports)
+	const cfg = module.exports
+	const versions = mgr.getVersions()
+	let versionsInfo
+	if (versions.length) {
+		versionsInfo = `<div class="versions">${versions
+			.map(
+				v => `<div class="version-info">
+<div class="version"><a href="/${v.VersionID}${v.StartURL}">${v.Title}</a></div>
+<div class="date">${getDate(v.Created)}</div>
+<hr>
+</div>`
+			)
+			.join('\n')}</div>`
+	} else {
+		versionsInfo = `
+<p>Welcome. Create a new prototype version by running:</p>
+<blockquote><code>protokit new</code></blockquote>
+`
+	}
+	return `<!doctype html>
+<meta charset=utf-8>
+<style>
+	html { margin: 0; padding: 0; }
+	body {
+		font-family: "HelveticaNeue", "Helvetica Neue", "Arial"; font-size: 18px;
+		margin: 0; padding: 0;
+	}
+	header { background: #0b0c0c; color: #fff; }
+	h1 { font-size: 48px; margin-bottom: 40px; }
+	h2 {
+		border-bottom: 4px solid rgb(43, 140, 196); font-size: 48px; font-weight: 300;
+		margin-bottom: 20px; margin-top: 60px;
+	}
+	hr { clear: both; width: 0; height: 0; line-height: 0; }
+	.center { text-align: center; }
+	.header-wrapper { margin: 0 auto; max-width: 965px; padding: 12px 15px; }
+	.header-wrapper img {
+		width: 36px; height: 32px; margin: 0 6px 0 0; vertical-align: top;
+	}
+	.header-wrapper strong { font-size: 30px; line-height: 35px; }
+	.main { margin: 0 auto; max-width: 965px; padding: 8px 15px; }
+	.version-info {
+		border-bottom: 1px dotted #ccc; padding: 24px 0 12px 0;
+	}
+	.versions { max-width: 800px; }
+	.versions a { color: #2578a8; }
+	.versions a:hover { color: #2b8cc4; }
+	.versions .version { float: left; width: 70%; }
+	.versions .date { float: left; width: 10%; color: #6f777b; }
+</style>
+<body>
+<header>
+	<div class="header-wrapper">
+		<img src="/static/base/crown.png">
+		<strong>${cfg.Org}</strong>
+	</div>
+</header>
+<div class="main">
+	<h1>${cfg.Title}</h1>
+	<p>These are the prototypes for ${cfg.Org}.</p>
+	<p><strong>Newest on top.</strong></p>
+	<h2>Current versions</h2>
+	${versionsInfo}
+</div>
+`
+}
+
 function serveStatic(ctx: web.Context, mgr: component.Manager, args: string[]) {
 	if (!args[0]) {
 		return 404
@@ -97,7 +179,7 @@ function serveStatic(ctx: web.Context, mgr: component.Manager, args: string[]) {
 	if (ctype) {
 		ctx.setHeader('content-type', ctype)
 	}
-	return fs.readFileSync(filepath, {encoding: 'utf8'})
+	return fs.readFileSync(filepath)
 }
 
 export async function main(args: optparse.Args) {
@@ -117,8 +199,8 @@ export async function main(args: optparse.Args) {
 		log.error(err)
 		process.exit(1)
 	}
-	log.info(
-		'>>',
+	log.setProcessMode()
+	log.success(
 		`Protokit server is running in ${env} mode at ${terminal.underline(
 			`http://${host || 'localhost'}:${port}`
 		)}`
