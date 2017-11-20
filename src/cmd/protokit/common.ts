414cf5de4d5d7eb6fcd39962244c02970da71607
tav <tav@espians.com>
2017-11-20 02:04:43 +0000

    cmd/protokit: extract common functionality into own module

---
+++ src/cmd/protokit/common.ts
@@ -0,0 +1,18 @@
+// Public Domain (-) 2017-present The Component Kit Authors.
+// See the Component Kit UNLICENSE file for details.
+
+import * as os from 'govuk/os'
+import * as path from 'path'
+
+export function getRoot() {
+	let dir = process.cwd()
+	while (true) {
+		if (os.isDirectory(path.join(dir, '.protokit'))) {
+			return [dir, '']
+		}
+		if (dir === '/') {
+			return ['', 'protokit: you do not seem to be inside a protokit repo']
+		}
+		dir = path.dirname(dir)
+	}
+}
