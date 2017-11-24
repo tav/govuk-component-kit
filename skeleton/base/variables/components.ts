7d47a941bd5642c92713d4dfd57701faebbe15e6
tav <tav@espians.com>
2017-11-24 13:26:04 +0000

    skeleton: add example variable files

---
+++ skeleton/base/variables/components.ts
@@ -0,0 +1,1 @@
+import {enumerate, range} from 'govuk/iter'
---
+++ skeleton/base/variables/css.ts
@@ -0,0 +1,8 @@
+const colours = {
+	HomeOffice: '#9325b2',
+}
+
+const gutterWidth = 30
+
+const gutter = `${gutterWidth}px`
+const gutterHalf = `${gutterWidth / 2}px`
