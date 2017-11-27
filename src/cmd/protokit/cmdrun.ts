51838ca1f9af3fac098ca2275c316329f3822539
tav <tav@espians.com>
2017-11-27 11:56:01 +0000

    cmd/protokit: split out template and implement shadow static

--- src/cmd/protokit/cmdrun.ts
+++ src/cmd/protokit/cmdrun.ts
@@ -3,7 +3,7 @@
 
 import * as fs from 'fs'
 import * as common from 'govuk/cmd/protokit/common'
-import * as component from 'govuk/componentkit/component'
+import * as protokit from 'govuk/componentkit/protokit'
 import * as log from 'govuk/log'
 import * as optparse from 'govuk/optparse'
 import * as os from 'govuk/os'
@@ -26,50 +26,29 @@ const CONTENT_TYPES: {[key: string]: string} = {
 	js: 'application/javascript',
 	png: 'image/png',
 	svg: 'image/svg+xml',
+	txt: 'text/plain',
 }
 
 function createRouter(root: string) {
-	let mgr: component.Manager
+	let mgr: protokit.Manager
 	try {
-		mgr = new component.Manager(root)
+		mgr = new protokit.Manager(root)
 	} catch (err) {
 		log.error(err)
 		process.exit(1)
 	}
 	return (ctx: web.Context) => {
 		const args = ctx.url.args
-		if (!args[0]) {
-			return serveRoot(ctx, mgr)
-		}
 		if (args[0] === 'static') {
 			return serveStatic(ctx, mgr, args.slice(1))
 		}
-		let page
 		try {
-			page = mgr.getPage(args)
+			return mgr.renderPage(ctx, args)
 		} catch (err) {
 			log.error(err)
 			return 500
 		}
-		if (!page) {
-			return 404
-		}
-		let resp
-		try {
-			resp = page.render(ctx, mgr)
-		} catch (err) {
-			log.error(err)
-			return 500
-		}
-		return resp
 	}
-}
-
-function getDate(timestamp: number) {
-	return new Date(timestamp)
-		.toISOString()
-		.slice(0, 10)
-		.replace(/-/g, '/')
 }
 
 function getDefaultPort() {
@@ -80,93 +59,35 @@ function getDefaultPort() {
 	return port
 }
 
-function serveRoot(ctx: web.Context, mgr: component.Manager) {
-	const cfgPath = path.join(mgr.root, 'site.ts')
-	const cfgFile = fs.readFileSync(cfgPath, {encoding: 'utf8'})
-	const module = {exports: {Org: '', Title: ''}}
-	new Function('module', 'exports', cfgFile)(module, module.exports)
-	const cfg = module.exports
-	const versions = mgr.versions || []
-	let versionsInfo
-	if (versions.length) {
-		versionsInfo = `<div class="versions">${versions
-			.map(
-				v => `<div class="version-info">
-<div class="version"><a href="/${v.VersionID}${v.StartURL}">${v.Title}</a></div>
-<div class="date">${getDate(v.Created)}</div>
-<hr>
-</div>`
-			)
-			.join('\n')}</div>`
-	} else {
-		versionsInfo = `
-<p>Welcome. Create a new prototype version by running:</p>
-<blockquote><code>protokit new</code></blockquote>
-`
-	}
-	return `<!doctype html>
-<meta charset=utf-8>
-<style>
-	html { margin: 0; padding: 0; }
-	body {
-		font-family: "HelveticaNeue", "Helvetica Neue", "Arial"; font-size: 18px;
-		margin: 0; padding: 0;
-	}
-	header { background: #0b0c0c; color: #fff; }
-	h1 { font-size: 48px; margin-bottom: 40px; }
-	h2 {
-		border-bottom: 4px solid rgb(43, 140, 196); font-size: 48px; font-weight: 300;
-		margin-bottom: 20px; margin-top: 60px;
-	}
-	hr { clear: both; width: 0; height: 0; line-height: 0; }
-	.center { text-align: center; }
-	.header-wrapper { margin: 0 auto; max-width: 965px; padding: 12px 15px; }
-	.header-wrapper img {
-		width: 36px; height: 32px; margin: 0 6px 0 0; vertical-align: top;
-	}
-	.header-wrapper strong { font-size: 30px; line-height: 35px; }
-	.main { margin: 0 auto; max-width: 965px; padding: 8px 15px; }
-	.version-info {
-		border-bottom: 1px dotted #ccc; padding: 24px 0 12px 0;
-	}
-	.versions { max-width: 800px; }
-	.versions a { color: #2578a8; }
-	.versions a:hover { color: #2b8cc4; }
-	.versions .version { float: left; width: 70%; }
-	.versions .date { float: left; width: 10%; color: #6f777b; }
-</style>
-<body>
-<header>
-	<div class="header-wrapper">
-		<img src="/static/base/crown.png">
-		<strong>${cfg.Org}</strong>
-	</div>
-</header>
-<div class="main">
-	<h1>${cfg.Title}</h1>
-	<p>These are the prototypes for ${cfg.Org}.</p>
-	<p><strong>Newest on top.</strong></p>
-	<h2>Current versions</h2>
-	${versionsInfo}
-</div>
-`
-}
-
-function serveStatic(ctx: web.Context, mgr: component.Manager, args: string[]) {
-	if (!args[0]) {
+function serveStatic(ctx: web.Context, mgr: protokit.Manager, args: string[]) {
+	if (args.length < 3) {
 		return 404
 	}
-	if (args[1] === 'components.css') {
-		const resp = mgr.getCSS(args[0])
-		if (!resp) {
+	const group = args[0]
+	const version = args[1]
+	args = args.slice(2)
+	if (args[2] === 'components.css') {
+		const resp = mgr.getCSS(group, version)
+		if (resp === undefined) {
 			return 404
 		}
 		ctx.setHeader('content-type', 'text/css')
 		return resp
 	}
-	const filepath = path.join(mgr.root, args[0], 'static', ...args.slice(1))
+	let filepath
+	if (group === 'base') {
+		filepath = path.join(mgr.root, 'base', 'static', ...args)
+	} else {
+		filepath = path.join(mgr.root, group, version, 'static', ...args)
+	}
 	if (!os.isFile(filepath)) {
-		return 404
+		if (group === 'base') {
+			return 404
+		}
+		filepath = path.join(mgr.root, 'base', 'static', ...args)
+		if (!os.isFile(filepath)) {
+			return 404
+		}
 	}
 	const ext = args[args.length - 1].split('.').pop() as string
 	const ctype = CONTENT_TYPES[ext]
@@ -181,7 +102,6 @@ export async function main(args: optparse.Args) {
 	const host = args.get('--host')
 	const env = args.includes('--prod') ? 'production' : 'dev'
 	const root = common.getRootOrExit()
-	log.setProcessMode()
 	const router = createRouter(root)
 	const opts = {env, staticBase: '/static/'}
 	const dispatcher = new server.Dispatcher(opts, router)
@@ -190,6 +110,7 @@ export async function main(args: optparse.Args) {
 		log.error(err)
 		process.exit(1)
 	}
+	console.log('')
 	log.success(
 		`Protokit server is running in ${env} mode at ${terminal.underline(
 			`http://${host || 'localhost'}:${port}`
