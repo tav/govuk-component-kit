5449c6ab42a185b674a6169139863e09fd84d2f0
tav <tav@espians.com>
2017-11-28 13:44:31 +0000

    componentkit: add function to flatten template output

--- src/componentkit/ckit.ts
+++ src/componentkit/ckit.ts
@@ -10,3 +10,19 @@ export function map<T>(iterable: Iterable<T>, callback: (elem: T) => string) {
 	}
 	return out
 }
+
+export function flatten(elems: any, result: string[] = [], top = true) {
+	for (const elem of elems) {
+		if (!elem) {
+			continue
+		}
+		if (typeof elem === 'string') {
+			result.push(elem)
+		} else {
+			flatten(elem, result, false)
+		}
+	}
+	if (top) {
+		return result.join('')
+	}
+}
